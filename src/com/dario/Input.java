a7ff610cc95fd145564f7e9b391ca1da87e9470e
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 10:17:49 +0530

    Input.java

--- src/com/dario/Input.java
+++ src/com/dario/Input.java
@@ -8,5 +8,11 @@ public class Input {
         System.out.print("Enter your Name: ");
         String name = sc.nextLine();
         System.out.println("Name: "+name);
+        System.out.print("Enter your Age: ");
+        int age = sc.nextInt();
+        System.out.println("Age: "+ age);
+        System.out.print("Enter your favorite programming lang: ");
+        String lang = sc.next();
+        System.out.println("Tech Stack: "+ lang);
     }
 }
