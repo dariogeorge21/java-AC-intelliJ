a76e97d0f45ae5926576f637cd0720880ba0629d
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 10:08:05 +0530

    Math lib

---
+++ src/com/dario/Maths.java
@@ -0,0 +1,14 @@
+package com.dario;
+
+public class Maths {
+    public static void main(String[] args) {
+        //max
+        System.out.println(Math.max(5,9));
+        //min
+        System.out.println(Math.min(5,9));
+        //random
+        System.out.println(Math.random());
+        //randomInt
+        System.out.println((int)(Math.random()*100));
+    }
+}
