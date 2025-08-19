c0dca22c92aa4e6fcc6340d55fc109fc4eadefb4
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 09:10:28 +0530

    datatypes

---
+++ src/com/dario/datatypes.java
@@ -0,0 +1,28 @@
+package com.dario;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class datatypes {
+    public static void main(String[] args) {
+//        Non primitive
+        String name = "Dario";
+        System.out.println(name.length());
+//        datatypes
+//        byte - 1;
+//        short - 2;
+//        int - 4;
+//        long -8;
+//        float - 4;
+//        double - 8;
+//        char - 2;
+//        boolean - 1;
+//        Primitive
+        byte age = 12;
+        short height = 883;
+        int num = 12;
+        long phoneNumber = 1001001001L;
+        float pi = 3.14F;
+        char letter = 'A';
+        boolean isAdult = true;
+    }
+}
