b2a1f12add19b2bfa0a5f515f940d3c04512adfa
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 10:19:35 +0530

    Comparison.java

---
+++ src/com/dario/Comparison.java
@@ -0,0 +1,14 @@
+package com.dario;
+
+public class Comparison {
+    public static void main(String[] args) {
+        int num1 = 23;
+        int num2 = 24;
+        System.out.println(num1==num2);
+        System.out.println(num1!=num2);
+        System.out.println(num1>=num2);
+        System.out.println(num2<=num1);
+        System.out.println(num1>num2);
+        System.out.println(num2<num1);
+    }
+}
