69f00836ade338ec98972655276a6f765fc58278
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 09:16:24 +0530

    concatenate

---
+++ src/com/dario/Strings.java
@@ -0,0 +1,10 @@
+package com.dario;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name1 = "Dario";
+        String name2 = "George";
+        String fullName = name1 + " " + name2;
+        System.out.println(fullName);
+    }
+}
