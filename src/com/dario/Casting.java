42500f65d3549280a26a96a58e3d1a5183052867
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 09:50:17 +0530

    type casting

---
+++ src/com/dario/Casting.java
@@ -0,0 +1,11 @@
+package com.dario;
+
+public class Casting {
+    public static void main(String[] args) {
+        double num = 100.0;
+        num+= (int)9.9;
+        int intNum = (int)num; //Type Casting
+        System.out.println("Int:" + intNum);
+        System.out.println("Double: " + num);
+    }
+}
