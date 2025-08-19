2df3c5faecdf619bceaf39a6c9f92b9bd45959b6
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 09:21:06 +0530

    replace

--- src/com/dario/Strings.java
+++ src/com/dario/Strings.java
@@ -1,10 +1,13 @@
 package com.dario;
 
+import java.sql.SQLOutput;
+
 public class Strings {
     public static void main(String[] args) {
         String name1 = "Dario";
         String name2 = "George";
         String fullName = name1 + " " + name2;
         System.out.println(fullName.replace("ario", "ariyo"));
+        System.out.println(fullName);
     }
 }
