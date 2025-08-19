0afe395937e8ffe916d5a16fca0b6e12d728adea
Dario George <edu.dariogeorge21@gmail.com>
2025-08-19 09:57:22 +0530

    operators

---
+++ src/com/dario/Operators.java
@@ -0,0 +1,18 @@
+package com.dario;
+
+public class Operators {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 6;
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int mul = num1 * num2;
+        int div = num2 / num1;
+        int mod = num1 % num2;
+        System.out.println("Sum: "+sum);
+        System.out.println("Difference: "+ diff);
+        System.out.println("Product: "+ mul);
+        System.out.println("Division: "+ div);
+        System.out.println("Modulo: "+ mod);
+    }
+}
